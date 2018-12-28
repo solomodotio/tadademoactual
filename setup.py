84cd5f7e564dd927c9c5d4521a3c4a1b63a29209
dansolomo <dan@solomo.io>
2018-12-28 16:07:23 -0600

    distribution package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Kaupter',
+    description='Kaupter integration platform',
+    version='1.2',
+    url='https://github.com/dansolomo/kaupter_base',
+    author='Dan Kauppi',
+    author_email='dan@solomo.import',
+    keywords=['integration','salesforce']
+    )
